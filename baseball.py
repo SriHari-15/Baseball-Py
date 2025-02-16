4d59ff8aeefc3a6d0c7b6863c40822b3c9b0b1ee
Sri Hari <71976594+SriHari-15@users.noreply.github.com>
2025-02-16 17:59:42 +0530

    Update baseball.py
    • Added "How to Play?" instructions
    • Added more comments
    • Added a "Strikes" counter for 3 Strikes
    • Fixed the game logic
    • Improved readability of code

--- baseball.py
+++ baseball.py
@@ -7,10 +7,10 @@ def get_input():
     while True:
         try:
             num = int(input("Choose a number --> 1 or 2: "))
-        except KeyboardInterrupt:
+        except KeyboardInterrupt: # Without this, you wont be able to stop the program when pressing CTRL + C
             print("\n-- Program stopped --")
             exit()
-        except ValueError:
+        except ValueError: # Prevents the program from closing when an input error occurs
             print("ERROR | Enter a valid number")
             continue
         
@@ -24,7 +24,7 @@ def get_input():
 def toss():
     print("-- TOSS --")
     while True:
-        usr_choice = input("Enter heads or tails: ").strip().upper()
+        usr_choice = input("Enter heads or tails: ").strip().upper() # Removes any spaces and converts to uppercase
         if usr_choice not in ("HEADS", "TAILS"):
             print("ERROR | Please enter 'Heads' or 'Tails'")
             continue
@@ -46,7 +46,17 @@ def toss():
 
 def game():
     print("--- BASEBALL ---\n")
+    print("> How to Play?")
+    print("- Toss will be held and you will choose either batting or pitching")
+    print("- If both players enter the same number, then the batting player gets 1 run")
+    print("- If they enter a different number, the batting player gets 1 Strike")
+    print("- When the batting player accumulates 3 consecutive Strikes, they are declared OUT and one innings is over")
+    print("- At the end of first innings, the final score of the batting player along with the target will be displayed")
+    print("- Now the pitching player will be batting and the same rule applies")
+    print("- If the batting player in the 2nd innings reaches the target, they WIN")
+    print("- If they get 3 Strikes and are OUT, they LOSE\n")
     
+    # Variables initialization
     usr_bat = toss()
     usr_score = 0
     c_score = 0
@@ -55,30 +65,41 @@ def game():
 
     print("\nYou are BATTING\n" if usr_bat else "\nYou are PITCHING\n")
     
+    counter = 1 # Strikes counter
+    
     while True:
         c_run = random.randint(1, 2)
         usr_run = get_input()
         print(f"Computer's choice: {c_run}")
 
-        if usr_run != c_run:
+        if usr_run == c_run:
+            counter = 1
             if usr_bat:
                 usr_score += 1
-                print(f"Your score: {usr_score}")
+                print(f"\n> Your score: {usr_score}\n")
             else:
                 c_score += 1
-                print(f"Computer's score: {c_score}")
+                print(f"\n> Computer's score: {c_score}\n")
         else:
-            print("<< OUT >>")
-            innings += 1
+            if counter == 3: # This statement is triggered only on 3 consecutive strikes
+                print(f"\n>> STRIKE: 3")
+                print("\n<< OUT >>")
+                innings += 1
+            else: # Increments the strike counter when there aren't 3 consecutive strikees
+                print(f"\n>> STRIKE: {counter}\n")
+                counter += 1
+                continue # To prevent the program from executing the bottom messages | They are reserved only for first innings ending
             
             if innings == 2:
                 break
 
+            # Would be better to put this in an if statement? if innings == 1 | Would improve readability, not sure
             print(f"\nYour Final Score: {usr_score}" if usr_bat else f"\nComputer's Final Score: {c_score}")
             target = usr_score + 1 if usr_bat else c_score + 1
-            print(f"\nTarget: {target}")
+            print(f">> TARGET: {target}")
             usr_bat = not usr_bat
             print("\nYou are BATTING\n" if usr_bat else "\nYou are PITCHING\n")
+            counter = 1 # To reset the counter after first innings
             continue
 
         if innings == 1 and ((usr_bat and usr_score >= target) or (not usr_bat and c_score >= target)): # Thanks ChatGPT :)
